f1298cd4da27e066eeb188a2d71a503336b48d98
Tim Landolt <timlandolt@gmx.ch>
2026-03-05 09:25:20 +0100

    Add gscus comments

--- .eleventy.js
+++ .eleventy.js
@@ -655,6 +655,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy({ "src/site/logo.*": "/" });
+  eleventyConfig.addPassthroughCopy("src/site/public");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
